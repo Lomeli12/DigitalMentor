72e906eba5c9e3ac3e5f3da74dd5ac11e35fc0c8
Anthony Lomeli <anthonylomeli11@gmail.com>
2022-05-11 10:50:09 -0700

    Setup the dev environment

---
+++ DigitalMentor/DigitalMentor.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Plugin;
+
+namespace DigitalMentor; 
+
+public class DigitalMentor : IDalamudPlugin {
+    public string Name => "Digital Mentor";
+    public DalamudPluginInterface pluginInterface { get; private set;  }
+
+    public DigitalMentor(DalamudPluginInterface pluginInterface) {
+        pluginInterface.Create<Services>();
+
+        this.pluginInterface = pluginInterface;
+    }
+    
+    public void Dispose() {
+    }
+}
