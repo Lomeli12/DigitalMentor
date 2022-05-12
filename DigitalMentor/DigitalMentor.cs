18f91d1ad1d01a4c99f31540a6e4d86e57a3c501
Anthony Lomeli <anthonylomeli11@gmail.com>
2022-05-11 17:16:18 -0700

    Basic command setup

--- DigitalMentor/DigitalMentor.cs
+++ DigitalMentor/DigitalMentor.cs
@@ -1,3 +1,4 @@
+using Dalamud.Game.Command;
 using Dalamud.Plugin;
 using DigitalMentor.Util;
 
@@ -17,8 +18,41 @@ public class DigitalMentor : IDalamudPlugin {
 
         this.config = (DigitalMentorConfig) pluginInterface.GetPluginConfig() ?? new DigitalMentorConfig();
         this.config.init(this, pluginInterface);
+        Services.Commands.AddHandler(Constants.commandName, new CommandInfo(handleDMCommand) {
+            HelpMessage = $@"{Constants.commandName} help - Display help options"
+        });
+    }
+
+    private void handleDMCommand(string command, string args) {
+        var arglist = args.Split(' ');
+        if (arglist.Length == 0) {
+            //TODO: Toggle tips on/off
+        } else {
+            switch (arglist[0]) {
+                case "enable":
+                case "on":
+                case "show":
+                    //TODO: Toggle tips on
+                    break;
+                case "disable":
+                case "off":
+                case "hide":
+                    //TODO: Toggle tips off
+                    break;
+                case "help":
+                case "?":
+                    //TODO: Display help information
+                    break;
+                case "options":
+                case "settings":
+                    Services.Chat.Print("Opening options window.");
+                    break;
+            }
+        }
+        Services.Chat.UpdateQueue();
     }
     
     public void Dispose() {
+        Services.Commands.RemoveHandler(Constants.commandName);
     }
 }
