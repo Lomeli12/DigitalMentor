e8fb8cb851abc7e822d9bb32ea6cc5777112348d
Anthony Lomeli <anthonylomeli11@gmail.com>
2022-05-11 17:14:00 -0700

    Added config

--- DigitalMentor/DigitalMentor.cs
+++ DigitalMentor/DigitalMentor.cs
@@ -1,15 +1,22 @@
-﻿using Dalamud.Plugin;
+using Dalamud.Plugin;
+using DigitalMentor.Util;
 
 namespace DigitalMentor; 
 
 public class DigitalMentor : IDalamudPlugin {
     public string Name => "Digital Mentor";
+    
     public DalamudPluginInterface pluginInterface { get; private set;  }
+
+    public DigitalMentorConfig config { get; private set; }
 
     public DigitalMentor(DalamudPluginInterface pluginInterface) {
         pluginInterface.Create<Services>();
 
         this.pluginInterface = pluginInterface;
+
+        this.config = (DigitalMentorConfig) pluginInterface.GetPluginConfig() ?? new DigitalMentorConfig();
+        this.config.init(this, pluginInterface);
     }
     
     public void Dispose() {
---
+++ DigitalMentor/Util/DigitalMentorConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+
+namespace DigitalMentor.Util;
+
+public class DigitalMentorConfig : IPluginConfiguration {
+
+    [NonSerialized] private DalamudPluginInterface pluginInterface;
+
+    [NonSerialized] private DigitalMentor digitalMentor;
+    
+    public int Version { get; set; } = 3;
+    public string language;
+
+    public void init(DigitalMentor digitalMentor, DalamudPluginInterface pluginInterface) {
+        this.digitalMentor = digitalMentor;
+        this.pluginInterface = pluginInterface;
+        
+    }
+
+    public void save() {
+        pluginInterface.SavePluginConfig(this);
+    }
+}
